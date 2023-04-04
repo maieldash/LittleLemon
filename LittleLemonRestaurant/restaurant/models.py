a231b7f77fac31f51bd0c30fc41f48a90e1eb827
Mai <maieldash0@gmail.com>
2023-04-04 13:21:00 +0200

    Menu Item page

--- LittleLemonRestaurant/restaurant/models.py
+++ LittleLemonRestaurant/restaurant/models.py
@@ -14,7 +14,7 @@ class MenuItem(models.Model):
    name = models.CharField(max_length=200)
    price = models.IntegerField()
    description = models.TextField(max_length=1000)
-   image = models.ImageField(upload_to = 'images',null= False,default='logo_footer.png')
+   image = models.ImageField(upload_to = 'restaurant\static\img\menu_items',null= False,default='logo_footer.png')
 
    def __str__(self):
        return self.name
